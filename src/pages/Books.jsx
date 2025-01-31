dabce870fc13a3c2dbea049713f2b78665e706f5
Shubhra D <shubhradwivedi21@gmail.com>
2025-01-31 20:36:16 +0530

    chande pages code

--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Books = () => {
   const [books, setBooks] = useState([]);
@@ -17,23 +18,37 @@ const Books = () => {
     setNewBook({ ...newBook, [e.target.name]: e.target.value });
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("New Book:", newBook);
+    //  API call to save the new book
+        axios({
+          url:`https://peppermint-carnelian-captain.glitch.me/books`,
+          method:"POST"
+        }).then((res)=>{
+          alert("Book Added Successfully")
+          fetchBooks()
+          setShowForm(false)
+          setNewBook({url:"",name:"",category:"",author:"",description:""})
+        }).catch((err)=>{
+          console.log(err)
+        }).finallly(()=>{
+          setIsLoading(false)
+        })
+  };
+
   const fetchBooks = () => {
     setIsLoading(true);
-    axios({
-      url: `https://peppermint-carnelian-captain.glitch.me/books`,
-      method: "GET",
-    })
+    axios
+      .get("https://peppermint-carnelian-captain.glitch.me/books")
       .then((res) => {
         setBooks(res.data);
         console.log(res.data);
       })
-      .catch((err) => {
-        console.error(err);
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
+      .catch((err) => console.error(err))
+      .finally(() => setIsLoading(false));
   };
+
   useEffect(() => {
     fetchBooks();
   }, []);
@@ -41,65 +56,62 @@ const Books = () => {
   return (
     <div>
       {isLoading ? (
-        <h1>Set is Loading!!!!!</h1>
+        <h1>Loading...</h1>
       ) : (
         <div>
           <h1>Book List</h1>
           {books.length > 0 &&
-            books.map((e) => {
-              return (
-                <ReactFragment>
-                  <div key={e.id}>
-                    <img src={e.coverImage} alt="book-image" />
-                    <h4>{e.name}</h4>
-                    <p>Genre : {e.category}</p>
-                    <p>Price : {e.price}</p>
-                    <p>Author : {e.author}</p>
-                    <p>Description : {e.description}</p>
-                    <Link
-                      to={`https://peppermint-carnelian-captain.glitch.me/books/${e.id}`}
-                    >
-                      View Details
-                    </Link>
-                  </div>
-                  {showForm ? (
-                    <form onSubmit={handleSubmit}>
-                      <input
-                        type="text"
-                        placeholder="Name of the Book"
-                        name="name"
-                        onChange={handleInput}
-                        value={newBook.name}
-                      />
-                      <input
-                        type="text"
-                        placeholder="Category"
-                        name="categroy"
-                        onChange={handleInput}
-                        value={newBook.category}
-                      />
-                      <input
-                        type="text"
-                        placeholder="Place the URL"
-                        name="url"
-                        onChange={handleInput}
-                        value={newBook.url}
-                      />
-                      <input
-                        type="text"
-                        placeholder="Description"
-                        name="description"
-                        onChange={handleInput}
-                        value={newBook.description}
-                      />
-                      <button type="submit">Add Book</button>
-                    </form>
-                  ) : (
-                    <button onClick={() => setShowForm(true)}>Add Book</button>
-                  )}
-                </ReactFragment>
-              );
-            })}
+            books.map((e) => (
+              <React.Fragment key={e.id}>
+                <div className="book-card">
+                  <img src={e.coverImage} alt="book-image" />
+                  <h4>{e.name}</h4>
+                  <p>Genre: {e.category}</p>
+                  <p>Price: {e.price}</p>
+                  <p>Author: {e.author}</p>
+                  <p>Description: {e.description}</p>
+                  <Link to={`https://peppermint-carnelian-captain.glitch.me/books/${e.id}`}>
+                    View Details
+                  </Link>
+                </div>
+              </React.Fragment>
+            ))}
+
+          {showForm ? (
+            <form onSubmit={handleSubmit}>
+              <input
+                type="text"
+                placeholder="Name of the Book"
+                name="name"
+                onChange={handleInput}
+                value={newBook.name}
+              />
+              <input
+                type="text"
+                placeholder="Category"
+                name="category"
+                onChange={handleInput}
+                value={newBook.category}
+              />
+              <input
+                type="text"
+                placeholder="Place the URL"
+                name="url"
+                onChange={handleInput}
+                value={newBook.url}
+              />
+              <input
+                type="text"
+                placeholder="Description"
+                name="description"
+                onChange={handleInput}
+                value={newBook.description}
+              />
+              <button type="submit">Add Book</button>
+            </form>
+          ) : (
+            <button onClick={() => setShowForm(true)}>Add Book</button>
+          )}
         </div>
       )}
     </div>
@@ -107,3 +119,4 @@ const Books = () => {
 };
 
 export default Books;
+
