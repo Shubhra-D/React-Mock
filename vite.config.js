8b15d463dc842a4045aa7db520e40d59a8fd4f01
Shubhra D <shubhradwivedi21@gmail.com>
2025-01-31 19:54:49 +0530

    trying

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/Book store/vite-project/React-Mock",
+  base:"/React-Mock",
   plugins: [react()],
 })
