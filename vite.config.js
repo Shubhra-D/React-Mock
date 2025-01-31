ce787978e5abb20551c4bf632e0b7017e5a42444
Shubhra D <shubhradwivedi21@gmail.com>
2025-01-31 18:34:42 +0530

    trying to deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/React-Mock",
+  base:"BOOK STORE/vite-project/React-Mock",
   plugins: [react()],
 })
